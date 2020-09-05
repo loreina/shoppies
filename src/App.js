cf9bdb0f4fafcd09ebf946c46fe10425180b5d10
loreina <loreina.chew@gmail.com>
2020-09-05 15:39:12 -0400

    feat: search omdb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+const OMDB_KEY = process.env.REACT_APP_OMDB_KEY;
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const updateSearch = () => {
+    searchMovies(search);
+  };
+
+  async function searchMovies(search) {
+    const res = await fetch(
+      `http://www.omdbapi.com/?apikey=${OMDB_KEY}&type=movie&s=${search}`
+    );
+    const data = await res.json();
+    setMovies(data);
+  }
+
+  useEffect(() => {
+    searchMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Search for a movie"
+        value={search}
+        onChange={handleChange}
+        onKeyDown={updateSearch}
+      />
+      <h1>{!!search ? `Results for "${search}"` : ""}</h1>
+      {movies.Search?.map((m) => (
+        <p>{!!search ? `${m.Title} (${m.Year})` : ""}</p>
+      ))}
     </div>
   );
 }
